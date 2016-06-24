3e49029f8c0e011dc34a7475fa0bad045d783e6b
Andrew <andrewday40@gmail.com>
2016-06-24 18:39:15 -0400

    cleanup from a mistake on another respoitory

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var server = express();
 
 server.use(express.static(__dirname+'/public'));
+// This is for CSS
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', { root: __dirname });
 });
+// This is for pointing to index.html for a webpage.
 
 server.listen(8080);
+// This is for localhost:8080
